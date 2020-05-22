8d9fd91b855a9bf221ca0f005671b2becab851e3
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-22 11:25:07 -0400

    sorted a linked list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection';
+import {LinkedList} from './LinkedList';
 
 const numbersCollection = new NumbersCollection([1000000, 3, -5, -18, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('FxleprqerkoDDLE');
 const sortCharacters = new Sorter(charactersCollection);
 sortCharacters.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkListSorter = new Sorter(linkedList);
+linkListSorter.sort();
+linkedList.print();
