606e6234a25d94fa98e1eed923600feaada4d7cb
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-22 10:45:16 -0400

    implimented sorting characters

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var sorter_1 = require("./sorter");
 var numbersCollection_1 = require("./numbersCollection");
-var numbersCollection = new numbersCollection_1.NumbersCollection([1000000, 3, -5, 0]);
+var CharactersCollection_1 = require("./CharactersCollection");
+var numbersCollection = new numbersCollection_1.NumbersCollection([1000000, 3, -5, -18, 0]);
 var sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('FxleprqerkoDDLE');
+var sortCharacters = new sorter_1.Sorter(charactersCollection);
+sortCharacters.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import {Sorter} from './sorter';
 import {NumbersCollection} from './numbersCollection';
+import {CharactersCollection} from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([1000000, 3, -5, 0]);
-
+const numbersCollection = new NumbersCollection([1000000, 3, -5, -18, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('FxleprqerkoDDLE');
+const sortCharacters = new Sorter(charactersCollection);
+sortCharacters.sort();
+console.log(charactersCollection.data);
