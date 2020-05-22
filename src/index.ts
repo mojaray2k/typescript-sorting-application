db812aa848bda8d629d2c6d7a5e9ac98983a611f
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-22 12:03:20 -0400

    made Sorter as an abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection';
 import {LinkedList} from './LinkedList';
 
 const numbersCollection = new NumbersCollection([1000000, 3, -5, -18, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('FxleprqerkoDDLE');
-const sortCharacters = new Sorter(charactersCollection);
-sortCharacters.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,6 +16,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const linkListSorter = new Sorter(linkedList);
-linkListSorter.sort();
+linkedList.sort();
 linkedList.print();
